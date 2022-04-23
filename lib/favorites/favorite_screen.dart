040ec2626c5328c7d9fff7805fe20e2387a9c533
Diego Arana Ballesteros <is709090@iteso.mx>
2022-04-23 17:06:55 -0500

    Finished UI of Favorite Screen

--- lib/favorites/favorite_screen.dart
+++ lib/favorites/favorite_screen.dart
@@ -23,36 +23,71 @@ class _favoriteScreenState extends State<favoriteScreen> {
           child: Container(
         width: MediaQuery.of(context).size.width * 0.95,
         height: MediaQuery.of(context).size.height * 0.4,
-        child: Card(
-            semanticContainer: true,
-            clipBehavior: Clip.antiAliasWithSaveLayer,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            elevation: 5,
-            margin: EdgeInsets.all(10),
-            child: Container(
-              decoration: new BoxDecoration(
-                image: new DecorationImage(
-                  image: new AssetImage("assets/images/soundWaveGif.gif"),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              child: Column(
-                children: [
-                  RawMaterialButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => favoriteScreen()));
-                    },
-                    elevation: 3.33,
-                    child: Icon(Icons.favorite, color: Colors.white),
+        child: GestureDetector(
+          onTap: () {
+            showDialog<String>(
+              context: context,
+              builder: (BuildContext context) => AlertDialog(
+                title: const Text('Abrir Canción'),
+                content: const Text(
+                    'Será redirigido a ver opciones para abrir la canción, ¿Quieres continuar?',
+                    textAlign: TextAlign.justify),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () => Navigator.pop(context, 'Cancelar'),
+                    child: const Text('Cancelar'),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Continuar'),
                   ),
                 ],
               ),
-            )),
+            );
+          },
+          child: Card(
+              semanticContainer: true,
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              elevation: 5,
+              margin: EdgeInsets.all(10),
+              child: Container(
+                decoration: new BoxDecoration(
+                  image: new DecorationImage(
+                    image: new AssetImage("assets/images/soundWaveGif.gif"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: RawMaterialButton(
+                        onPressed: () {},
+                        elevation: 3.33,
+                        child: Icon(Icons.favorite, color: Colors.white),
+                      ),
+                    ),
+                    Spacer(),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.08,
+                      width: double.maxFinite,
+                      color: Colors.blue,
+                      child: Text(
+                        "Titulo de la canción" + "\n" + "Artista de la canción",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 23,
+                            height: 1.3,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              )),
+        ),
       )),
     );
   }
